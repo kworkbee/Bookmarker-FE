536034af0bb711ad8d78c06af7273022e6bd3827
G1 <g1.tommy.jeon@gmail.com>
2022-09-04 23:39:45 +0900

    feat: Set tailwindCSS, Configuration Up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,28 @@
+import path from "path";
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  server: {
+    port: 3000
+  },
+  plugins: [
+    svelte({
+      compilerOptions: {
+        dev: true
+      }
+    })
+  ],
+  assetsInclude: ["**/*.png", "**/*.ico"],
+  resolve: {
+    alias: {
+      "@/": `${path.resolve(__dirname, "src")}/`,
+    },
+    dedupe: ["svelte"]
+  },
+  optimizeDeps: {
+    exclude: ["@roxi/routify"]
+  },
+  clearScreen: false,
 })
